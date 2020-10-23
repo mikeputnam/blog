7e7d553fc4ff93c749df07fa4d91be8678be6b95
Mike Putnam <mike@theputnams.net>
2020-10-23 17:53:50 -0500

    Refactor

--- mike/staging/build.sh
+++ mike/staging/build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-cd posts/ #invoked from staging/
-#assemble index from HTML chunks
-html="<ul>"
-for post in $(find * | sort -r);do html=$html"<li><a href=\"$post.htm\">$post</a></li>"; done
-html=$html"</ul>"
-printf '%s\n' "$html" | cat ../top.template - ../bot.template > ../../index.html
-#assemble all the blog pages from all the chunks
-for post in $(find * | sort -r);do cat ../top.template $post ../bot.template > ../../$post.htm; done
-#assemble atom feed from all the chunks
+cd posts/ # invoked from staging/
+# assemble post index from HTML chunks in posts/
+postindexhtml="<!-- BEGIN post-index -->"
+postindexhtml=$postindexhtml"<u>FILES</u>"
+# 3 most recent posts are visible
+for post in $(find * | sort -r | head -n 3 );do postindexhtml=$postindexhtml"<b><span class=\"fil\"    ><a href=\"$post.htm\">$post</a></span></b>"; done
+# the rest are not visible; "hid"den.
+for post in $(find * | sort -r | tail -n +3);do postindexhtml=$postindexhtml"<b><span class=\"fil hid\"><a href=\"$post.htm\">$post</a></span></b>"; done
+postindexhtml=$postindexhtml"<!-- END post-index -->"
+printf '%s\n' "$postindexhtml" | cat ../top-template.htm ../index-copy.htm ../bottom-above-posts-template.htm - ../bottom-below-posts-template.htm > ../../index.html
+# assemble all the blog pages from all the chunks
+for post in $(find * | sort -r);do printf '%s\n' "$postindexhtml" | cat ../top-template.htm $post ../bottom-above-posts-template.htm - ../bottom-below-posts-template.htm > ../../$post.htm; done
+# assemble atom feed from all the chunks
 atom_header=$(cat <<EOM
 <?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
